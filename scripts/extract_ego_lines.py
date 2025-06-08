f9d1e59221128c09c7265d403548f528afcab745
Stephen Edwards <sje.txt@gmail.com>
2025-06-08 14:51:18 +0300

    add sarra 0, marrak 0

--- scripts/extract_ego_lines.py
+++ scripts/extract_ego_lines.py
@@ -6,35 +6,35 @@ LINE_PATH = os.path.join("..", "lines")
 
 
 def line_to_key(line):
-    key = line
-    key = key.replace("'", "")
-    key = key.replace(".", "")
-    key = key.replace(" ", "_")
-    key = key.replace("-", "_")
-    key = key.replace("&", "")
-    key = key.replace("\n", "")
-    key = key.lower()
-    return key
+	key = line
+	key = key.replace("'", "")
+	key = key.replace(".", "")
+	key = key.replace(" ", "_")
+	key = key.replace("-", "_")
+	key = key.replace("&", "")
+	key = key.replace("\n", "")
+	key = key.lower()
+	return key
 
 def extract_ego():
-    txtpath = os.path.join(LINE_PATH, "ego", "all.txt")
-    if not os.path.isfile(txtpath):
-        print("ERROR: not a real file {}".format(txtpath))
-        return
-    lines = list()
-    with open(txtpath, "r") as file:
-        lines = file.readlines()
+	txtpath = os.path.join(LINE_PATH, "ego", "all.txt")
+	if not os.path.isfile(txtpath):
+		print("ERROR: not a real file {}".format(txtpath))
+		return
+	lines = list()
+	with open(txtpath, "r") as file:
+		lines = file.readlines()
 
-    data = dict()
+	data = dict()
 
-    for l in lines:
-        key = line_to_key(l)
-        newl = l.replace("\n", "")
-        data[line_to_key(l)] = newl
+	for l in lines:
+		key = line_to_key(l)
+		newl = l.replace("\n", "")
+		data[line_to_key(l)] = newl
 
-    outpath = os.path.join(LINE_PATH, "ego", "en_us.json")
-    with open(outpath, "w") as file:
-        json.dump(data, file, indent=4, sort_keys=True)
+	outpath = os.path.join(LINE_PATH, "ego", "en_us.json")
+	with open(outpath, "w") as file:
+		json.dump(data, file, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
-    extract_ego()
+	extract_ego()
--- scripts/verify_dialogs.py
+++ scripts/verify_dialogs.py
@@ -7,92 +7,96 @@ LINE_PATH = os.path.join("..", "lines")
 
 TOP_LEVEL_KEYS = set(["intro", "prompts"])
 RESPONSE_KEYS = set(["speaker", "line"])
-PROMPT_KEYS = set(["prompt", "response", "cycle"])
+PROMPT_KEYS = set(["prompt", "response", "cycle", "subprompts"])
 
 
 def read_all_lines():
-    lines = dict()
-    for p in os.listdir(LINE_PATH):
-        linepath = os.path.join(LINE_PATH, p, "en_us.json")
-        with open(linepath, 'r') as file:
-            lines[p] = json.load(file)
-    return lines
+	lines = dict()
+	for p in os.listdir(LINE_PATH):
+		linepath = os.path.join(LINE_PATH, p, "en_us.json")
+		with open(linepath, 'r') as file:
+			lines[p] = json.load(file)
+	return lines
 
 def read_all_dialogs():
-    dialogs = dict()
-    for p in os.listdir(DIALOG_PATH):
-        dpath = os.path.join(DIALOG_PATH, p)
-        with open(dpath, 'r') as file:
-            dialogs[p] = json.load(file)
-    return dialogs
+	dialogs = dict()
+	for p in os.listdir(DIALOG_PATH):
+		dpath = os.path.join(DIALOG_PATH, p)
+		with open(dpath, 'r') as file:
+			dialogs[p] = json.load(file)
+	return dialogs
 
 def verify_line(speaker, key, lines):
-    # TODO: separate characters from lines?
-    if not speaker in lines:
-        print("invalid speaker {}".format(speaker))
-        return False
-    if not key in lines[speaker]:
-        print("line {} not found in lines for {}".format(key, speaker))
-        return False
-    print("{} : {} : OK!".format(speaker, key))
-    return True
+	if not speaker in lines:
+		print("invalid speaker {}".format(speaker))
+		return False
+	if not key in lines[speaker]:
+		print("line {} not found in lines for {}".format(key, speaker))
+		return False
+	# print("{} : {} : OK!".format(speaker, key))
+	return True
 
 def verify_response(response, lines):
-    if set(response.keys()) != RESPONSE_KEYS:
-        print("invalid response keys: {}".format([response.keys()]))
-        return False
-    return verify_line(response["speaker"], response["line"], lines)
+	if set(response.keys()) != RESPONSE_KEYS:
+		print("invalid response keys: {}".format([response.keys()]))
+		return False
+	return verify_line(response["speaker"], response["line"], lines)
 
 def verify_prompt(prompt, lines):
-    for key, value in prompt.items():
-        if key not in PROMPT_KEYS:
-            print("invalid prompt key {}".format(key))
-            return False
+	for key, value in prompt.items():
+		if key not in PROMPT_KEYS:
+			print("invalid prompt key {}".format(key))
+			return False
 
-        if key == "prompt":
-            if value in lines["ego"]:
-                print("ego : {} OK!".format(value))
-            else:
-                print("invalid prompt {}".format(value))
-                return False
+		if key == "prompt":
+			if value in lines["ego"]:
+				pass
+				# print("ego : {} OK!".format(value))
+			else:
+				print("invalid prompt {}".format(value))
+				return False
 
-        if key == "response" and not verify_response(value, lines):
-            return False
+		if key == "response" or key == "cycle":
+			if not type(value) == list:
+				print("invalid {} value: {}", key, value)
+				return False
+			for response in value:
+				if not verify_response(response, lines):
+					return False
 
-        if key == "cycle":
-            if not type(value) == list:
-                print("invalid cycle value: {}", value)
-                return False
-            for response in value:
-                if not verify_response(response, lines):
-                    return False
-
-    return True
+		if key == "subprompts":
+			if not type(value) == list:
+				print("invalid {} value: {}", key, value)
+				return False
+			for subprompt in value:
+				if not verify_prompt(subprompt, lines):
+					return False
+	return True
 
 def verify(dialog, lines):
-    for key in dialog.keys():
-        if key not in TOP_LEVEL_KEYS:
-            print("invalid key {}".format(key))
-            return False
+	for key in dialog.keys():
+		if key not in TOP_LEVEL_KEYS:
+			print("invalid key {}".format(key))
+			return False
 
-    if "intro" in dialog:
-        for response in dialog["intro"]:
-            if not verify_response(response, lines):
-                return False
+	if "intro" in dialog:
+		for response in dialog["intro"]:
+			if not verify_response(response, lines):
+				return False
 
-    for prompt in dialog.get("prompts", []):
-        if not verify_prompt(prompt, lines):
-            return False
+	for prompt in dialog.get("prompts", []):
+		if not verify_prompt(prompt, lines):
+			return False
 
-    return True
+	return True
 
 if __name__ == "__main__":
-    lines = read_all_lines()
-    dialogs = read_all_dialogs()
-    for key, dialog in dialogs.items():
-        valid = verify(dialog, lines)
-        if (valid):
-            print("dialog {} OK!".format(key))
-        else:
-            print("dialog {} INVALID!!!".format(key))
-        print("============================")
+	lines = read_all_lines()
+	dialogs = read_all_dialogs()
+	for key, dialog in dialogs.items():
+		valid = verify(dialog, lines)
+		if (valid):
+			print("dialog {} OK!".format(key))
+		else:
+			print("dialog {} INVALID!!!".format(key))
+		print("============================")
