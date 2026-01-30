9f6420115449d9a5d99af34a1ea117eb6d36b03b
Stephen Edwards <sje.txt@gmail.com>
2026-01-30 14:26:43 +0200

    rip messages works

--- rip/ripmsg.py
+++ rip/ripmsg.py
@@ -14,7 +14,7 @@ def to_short(two_bytes):
 	return value
 
 def to_msg_id(b13):
-	return "".join([char(b) for b in b13])
+	return "".join([chr(b) for b in b13])
 
 """
 1. split data into 4-byte chunks and tail 0-3 bytes long;
@@ -27,27 +27,24 @@ def to_msg_id(b13):
 """
 def decode(msg):
 	deffed = list()
-	tail = len(msg) % 4
-	for i in range(0, len(msg) - tail, 4):
+	tailstart = len(msg) - (len(msg) % 4)
+	for i in range(0, tailstart, 4):
 		step2 = msg[i:i+4]
 		step3 = to_int(step2)
 
 		step4 = (step3 ^ 0xf1acc1d) & 0xffffffff
 
-		step5 = ((step4 << 15) | (step4 >> 17)) & 0xffffffff
+		step5 = ((step4 >> 15) | (step4 << 17)) & 0xffffffff
 
 		deffed.append(chr(0xff & (step5 >> 0)))
 		deffed.append(chr(0xff & (step5 >> 8)))
 		deffed.append(chr(0xff & (step5 >> 16)))
 		deffed.append(chr(0xff & (step5 >> 24)))
 
-	print(deffed)
+	for i in range(tailstart, len(msg)):
+		deffed.append(chr(msg[i] ^ 0xff))
 
-	'''
-	for i in range(msg_len - tail, msg_len):
-		deffed.append(~file.read(1))
-	'''
-	return "{}".format(deffed)
+	return "".join(deffed)
 
 def extract_blocks(fpath):
 	with open(fpath, 'rb') as file:
@@ -67,43 +64,43 @@ def extract_blocks(fpath):
 			print("MSG BLOCK {}======".format(block_num))
 			num_options = to_short(file.read(2))
 			flags = to_short(file.read(2))
-			unknown_1 = to_short(file.read(2))
-			msg_num = to_short(file.read(2))
-			msg_len = to_short(file.read(2))
-			unknown_2 = to_short(file.read(2))
-			msg_lbl_present = to_short(file.read(2))
-			unknown_3 = to_short(file.read(2))
+			# msg_num = to_short(file.read(2))
+			# unknown_1 = to_short(file.read(2))
+			# msg_len = to_short(file.read(2))
+			# unknown_2 = to_short(file.read(2))
+			# msg_lbl_present = to_short(file.read(2))
+			# unknown_3 = to_short(file.read(2))
+			msg_num = to_int(file.read(4))
+			msg_len = to_int(file.read(4))
+			msg_lbl_present = to_int(file.read(4))
+
 			print("id: {}".format(block_id))
 			print("char: {}".format(block_meta[0]))
-			print("options: {}".format(num_options))
-			print("flags: {}".format(flags))
-			print("msg_num: {}".format(msg_num))
-			print("msg_len: {}".format(msg_len))
-			print("msg_lbl_present: {}".format(msg_lbl_present))
-			print("unknown: {} {} {}".format(unknown_1, unknown_2, unknown_3))
 
 			if msg_lbl_present > 0:
-				msg_id = file.read(13)
+				msg_id = to_msg_id(file.read(13))
 				print("msg id: {}".format(msg_id))
 
 			for option_num in range(num_options):
-				option_id = file.read(13)
+				option_id = to_msg_id(file.read(13))
 				print("option_id: {}".format(option_id))
 
-			"""
-			if flags & 0x04:
-				msg = decode(file, msg_len)
-			else:
+			if (msg_len > 0):
 				msg = file.read(msg_len)
-			print("{}".format(msg))
-			"""
+				if flags & 0x04:
+					decoded = decode(msg)
+				else:
+					decoded = "".join(msg)
+				print("msg: {}".format(decoded))
+			# print("options: {}".format(num_options))
+			print("flags: {}".format(flags))
+			print("msg_num: {}".format(msg_num))
+			print("msg_len: {}".format(msg_len))
+			# print("msg_lbl_present: {}".format(msg_lbl_present))
+			# print("unknown: {} {} {}".format(unknown_1, unknown_2, unknown_3))
 
-			msg = file.read(msg_len)
-			if flags & 0x04:
-				decode(msg)
-
-			footer = file.read(4)
-			print("{}\n".format(to_int(footer)))
+			footer = to_int(file.read(4))
+			print("{}======\n".format(footer))
 
 
 if __name__ == "__main__":
