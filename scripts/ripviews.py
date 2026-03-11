f673937ce0682a5a68f62e9f4eb9e02ca193a15a
Stephen Edwards <sje.txt@gmail.com>
2026-03-11 14:21:52 +0200

    implement rmap

--- scripts/ripviews.py
+++ scripts/ripviews.py
@@ -4,6 +4,8 @@ from PIL import Image
 sys.path.append(".")
 from riputils import NPC_HEADS
 
+LEN_RPAL = 72
+
 def read_int(f):
     return int.from_bytes(f.read(4), 'little')
 
@@ -55,7 +57,27 @@ def read_mode_1(f, h, w, n):
         dep.append(read_byte(f))
     return arr
 
-def read_collection(f, palette, mode):
+def create_img(num, w, h, arr, palette, rpal, pmap):
+    img = Image.new('P', (w, h))
+    if pmap:
+        img.putpalette(rpal, 'RGB')
+    else:
+        img.putpalette(palette, 'RGB')
+
+    for x in range(w):
+        for y in range(h):
+            index = (num * h * w) + (y * w) + x
+            if index >= len(arr):
+                print("invalid index attempted: {} > {}".format(index, len(arr)))
+                return img
+            if pmap:
+                pixel = pmap[arr[index]]
+            else:
+                pixel = arr[index]
+            img.putpixel((x, y), pixel)
+    return img
+
+def read_collection(f, palette, mode, pmap):
     x = read_int(f)
     y = read_int(f)
     w = read_int(f)
@@ -85,21 +107,12 @@ def read_collection(f, palette, mode):
     
     imgs = []
     for k in range(n):
-        img = Image.new('P', (w, h))
-        img.putpalette(palette, 'RGB')
-        for i in range(w):
-            for j in range(h):
-                index = (k * h * w) + (j * w) + i
-                if index >= len(arr):
-                    print("invalid index attempted: {} > {}".format(index, len(arr)))
-                    return imgs
-                img.putpixel((i, j), arr[index])
-        imgs.append(img)
+        imgs.append(create_img(k, w, h, arr, palette, rpal, pmap))
     return imgs
 
-def process_file(filename):
+def process_file(filename, rpal):
     outdir = os.path.join('..', 'rip', 'views', os.path.basename(filename).split('.')[0])
-    if (not os.path.isdir(outdir)):
+    if not os.path.isdir(outdir):
         os.mkdir(outdir)
 
     with open(filename, "rb") as f:
@@ -107,25 +120,59 @@ def process_file(filename):
         num_collections = read_int(f)
         print("{}: {} collections encoded with mode {}".format(filename, num_collections, mode))
         palette = read_palette(f)
+        pmap = get_pmap(palette, rpal)
 
         # offsets
         for _ in range(num_collections):
             read_int(f)
 
         for c in range(num_collections):
-            imgs = read_collection(f, palette, mode)
+            imgs = read_collection(f, palette, mode, pmap)
             i = 0
             for img in imgs:
-                outpath = os.path.join(outdir, "{}v{}.png".format(c, i))
+                if rpal:
+                    outpath = os.path.join(outdir, "r{}v{}.png".format(c, i))
+                else:
+                    outpath = os.path.join(outdir, "{}v{}.png".format(c, i))
                 img.save(outpath)
                 i += 1
 
+def get_rpal():
+    path = os.path.join('..', 'rip', 'views', '999.bmp')
+    if not os.path.isfile(path):
+        print('invalid remap palette location: {}'.format(path))
+        return []
+    rpal = Image.open(path).getpalette()
+    for i in range(LEN_RPAL * 3, len(rpal)):
+        rpal[i] = 0
+    return rpal
+
+def get_pmap(pal, rpal):
+    if not rpal:
+        return dict()
+
+    pmap = dict()
+    for i in range(256):
+        best = 256 * 256 * 3
+        best_index = 0
+        for j in range(LEN_RPAL):
+            dist = (pal[i * 3] - rpal[j * 3]) ** 2 + (pal[i * 3 + 1] - rpal[j * 3 + 1]) ** 2 + (pal[i * 3 + 2] - rpal[j * 3 + 2]) ** 2
+            if dist < best:
+                best_index = j
+                best = dist
+        pmap[i] = best_index
+    pmap[255] = 255
+    return pmap
+
 if __name__ == "__main__":
-    # for headnum in range(5, 73):
-    for headnum in range(20, 22):
+    rpal = []
+    if "remap" in sys.argv:
+        rpal = get_rpal()
+
+    for headnum in range(5, 73):
         filename = "{}.GRA".format(100000 + headnum)
         path = os.path.join('..', 'rip', 'CDN', 'GRA', "{}.GRA".format(100000 + headnum))
         if os.path.isfile(path):
-            process_file(path)
+            process_file(path, rpal)
         else:
             print("404 {}".format(path))
